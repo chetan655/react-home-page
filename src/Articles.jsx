f2cb0ac96255a7b929e2ffd6e49c751e309545f7
chetan655 <chetannagar655@gmail.com>
2022-09-01 16:32:16 +1000

    added-map-functions

--- src/Articles.jsx
+++ src/Articles.jsx
@@ -3,29 +3,21 @@ import ArticleItems from "./ArticleItems";
 import { ArticleCards } from "./Cards"
 import './card.css'
 
+function articleCardComponent(article, i){
+    return  <ArticleItems 
+        image = {article.image}
+        header = {article.header}
+        meta = {article.meta}
+        description = {article.description}
+        extra = {article.extra}/>
+}
+
 function Articles(){
     return(
         <div className="articles-comp">
             <p className='cards-heading'>Featured Articles</p>
                 <div className="row">
-                    <ArticleItems 
-                    image = {ArticleCards[0].image}
-                    header = {ArticleCards[0].header}
-                    meta = {ArticleCards[0].meta}
-                    description = {ArticleCards[0].description}
-                    extra = {ArticleCards[0].extra}/>
-                    <ArticleItems 
-                    image = {ArticleCards[1].image}
-                    header = {ArticleCards[1].header}
-                    meta = {ArticleCards[1].meta}
-                    description = {ArticleCards[1].description}
-                    extra = {ArticleCards[1].extra}/>
-                    <ArticleItems 
-                    image = {ArticleCards[2].image}
-                    header = {ArticleCards[2].header}
-                    meta = {ArticleCards[2].meta}
-                    description = {ArticleCards[2].description}
-                    extra = {ArticleCards[2].extra}/>
+                    {ArticleCards.map(articleCardComponent)}
                 </div>
             <button className="articles-button">See All Articles</button>
         </div>
--- src/Tutorials.jsx
+++ src/Tutorials.jsx
@@ -3,29 +3,21 @@ import TutorialItems from "./TutorialItems";
 import { TutorialCards } from "./Cards"
 import './card.css'
 
+function tutorialCardComponent(tutorial, i){
+    return  <TutorialItems 
+        image = {tutorial.image}
+        header = {tutorial.header}
+        meta = {tutorial.meta}
+        description = {tutorial.description}
+        extra = {tutorial.extra}/>
+}
+
 function Tutorials(){
     return(
         <div className="tutorials-comp">
             <p className='cards-heading'>Featured Tutorials</p>
                 <div className="row">
-                    <TutorialItems 
-                    image = {TutorialCards[0].image}
-                    header = {TutorialCards[0].header}
-                    meta = {TutorialCards[0].meta}
-                    description = {TutorialCards[0].description}
-                    extra = {TutorialCards[0].extra}/>
-                    <TutorialItems 
-                    image = {TutorialCards[1].image}
-                    header = {TutorialCards[1].header}
-                    meta = {TutorialCards[1].meta}
-                    description = {TutorialCards[1].description}
-                    extra = {TutorialCards[1].extra}/>
-                    <TutorialItems 
-                    image = {TutorialCards[2].image}
-                    header = {TutorialCards[2].header}
-                    meta = {TutorialCards[2].meta}
-                    description = {TutorialCards[2].description}
-                    extra = {TutorialCards[2].extra}/>
+                    {TutorialCards.map(tutorialCardComponent)}
                 </div>
             <button className="tutorials-button">See All Tutorials</button>
         </div>
